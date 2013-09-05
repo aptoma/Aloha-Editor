b1e7e6af94ce262f1fb75d84e5103fcb345cfb6f
Patrik Henningsson <patrik.henningsson@gmail.com>
2013-09-05 13:25:01 +0200

    Rewrite undo-plugin and see if we can get rid of all weird stuff happening.

--- src/plugins/common/undo/lib/undo-plugin.js
+++ src/plugins/common/undo/lib/undo-plugin.js
@@ -1,9 +1,9 @@
 /* undo-plugin.js is part of Aloha Editor project http://aloha-editor.org
  *
- * Aloha Editor is a WYSIWYG HTML5 inline editing library and editor. 
+ * Aloha Editor is a WYSIWYG HTML5 inline editing library and editor.
  * Copyright (c) 2010-2012 Gentics Software GmbH, Vienna, Austria.
- * Contributors http://aloha-editor.org/contribution.php 
- * 
+ * Contributors http://aloha-editor.org/contribution.php
+ *
  * Aloha Editor is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
@@ -17,149 +17,230 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * 
+ *
  * As an additional permission to the GNU GPL version 2, you may distribute
  * non-source (e.g., minimized or compacted) forms of the Aloha-Editor
  * source code without the copy of the GNU GPL normally required,
  * provided you include this license notice and a URL through which
  * recipients can access the Corresponding Source.
  */
-define(
-['aloha', 'jquery', 'aloha/plugin', 'undo/vendor/undo', 'undo/vendor/diff_match_patch_uncompressed'],
-function( Aloha, jQuery, Plugin) {
-	"use strict";
-	var
-		dmp = new diff_match_patch,
-		resetFlag = false;
+/* globals Undo */
+define(function (require) {
+	'use strict';
 
-	function reversePatch(patch) {
-		var reversed = dmp.patch_deepCopy(patch);
-		for (var i = 0; i < reversed.length; i++) {
-			for (var j = 0; j < reversed[i].diffs.length; j++) {
-				reversed[i].diffs[j][0] = -(reversed[i].diffs[j][0]);
-			}
+	/**
+	 * Module dependencies.
+	 */
+	var Aloha = require('aloha'),
+		Plugin = require('aloha/plugin'),
+		Ui = require('ui/ui'),
+		Button = require('ui/button'),
+		rangy = require('aloha/rangy-core'),
+		$ = require('jquery');
+
+	require('undo/vendor/undo');
+
+	/**
+	 * Restore blocks in the active editor.
+	 */
+	function enableBlocks() {
+		if (!Aloha.settings.plugins.block.defaults) {
+			Aloha.settings.plugins.block.defaults = {};
 		}
-		return reversed;
+
+		var editable = Aloha.getActiveEditable();
+
+		if (editable !== null) {
+			var $editor = $(editable.obj);
+
+			$.each(Aloha.settings.plugins.block.defaults, function (selector, instanceDefaults) {
+				$editor.find(selector).alohaBlock(instanceDefaults);
+			});
+		}
 	}
 
 	/**
-	 * register the plugin with unique name
+	 * Undo/redo command.
+	 * @type Object
 	 */
-	return Plugin.create('undo', {
+	var EditCommand = Undo.Command.extend({
+		constructor: function (editable, content, bookmark) {
+			this.editable = editable;
+			this.content = content;
+			this.bookmark = bookmark;
+		},
+
+		execute: function () {},
+
+		undo: function () {
+			plugin.setContent(this.editable, this.content, this.bookmark);
+		},
+
+		redo: function () {
+			plugin.setContent(this.editable, this.content, this.bookmark);
+		},
+	});
+
+	/**
+	 * Register the plugin with unique name.
+	 */
+	var plugin = Plugin.create('undo', {
+
+		undoInProgress: false,
+		snaphotInProgress: false,
+
 		/**
-		 * Initialize the plugin and set initialize flag on true
+		 * Initialize the plugin and set initialize flag on true.
 		 */
 		init: function () {
-			this.stack = new Undo.Stack();
-			var that = this;
-			var EditCommand = Undo.Command.extend({
-					constructor: function(editable, patch) {
-						this.editable = editable;
-						this.patch = patch;
-					},
-					execute: function() {
-						//command object is created after execution.
-					},
-					undo: function() {
-						this.phase(reversePatch(this.patch));
-					},
-					redo: function() {
-						this.phase(this.patch);
-					},
-					phase: function(patch) {
-						var contents = this.editable.getContents(),
-							applied = dmp.patch_apply(patch, contents),
-							newValue = applied[0],
-							didNotApply = applied[1];
-						if (didNotApply.length) {
-							//error
-						}
-						this.reset(newValue);
-					},
-					reset: function(val) {
-						//we have to trigger a smartContentChange event
-						//after doing an undo or redo, but we mustn't
-						//push new commands on the stack, because there
-						//are no new commands, just the old commands on
-						//the stack that are undone or redone.
-						resetFlag = true;
+			plugin.stack = new Undo.Stack();
 
-						var reactivate = null;
-						if (Aloha.getActiveEditable() === this.editable) {
-							Aloha.deactivateEditable();
-							reactivate = this.editable;
-						}
-
-						this.editable.obj.html(val);
-
-						if (null !== reactivate) {
-							reactivate.activate();
-						}
-
-						//TODO: this is a call to an internal
-						//function. There should be an API to generate
-						//new smartContentChangeEvents.
-						this.editable.smartContentChange({type : 'blur'});
-
-						resetFlag = false;
-					}
-				});
-
-			this.stack.changed = function() {
-				// update UI
+			plugin.stack.changed = function () {
+				plugin.updateButtons();
 			};
 
-			Aloha.bind('aloha-editable-created', function(e, editable){
-				editable.obj.bind('keydown', 'ctrl+z shift+ctrl+z', function(event){
+			plugin.createButtons();
+
+			Aloha.bind('aloha-editable-created', function (e, editable) {
+				editable.obj.bind('keydown', 'ctrl+z meta+z ctrl+shift+z meta+shift+z', function (event) {
 					event.preventDefault();
 					if (event.shiftKey) {
-						that.redo();
+						plugin.redo();
 					} else {
-						that.undo();
+						plugin.undo();
 					}
 				});
 			});
 
-			Aloha.bind('aloha-smart-content-changed', function(jevent, aevent) {
-				// The editable only actually makes a snapshot when
-				// getSnapshotContent is called, so we need to call it now
-				// to ensure such a snapshot is made at all times, even when
-				// resetFlag===true, otherwise the snapshot grows stale.
-				var oldValue = aevent.getSnapshotContent();
+			Aloha.bind('aloha-smart-content-changed', function (event, obj) {
+				plugin.stack.execute(new EditCommand(
+					obj.editable,
+					obj.editable.undoSnapshotContent,
+					obj.editable.undoSnapshotBookmark
+				));
+				plugin.snaphotInProgress = false;
+			});
 
-				if (resetFlag) {
-					return;
+			Aloha.bind('aloha-command-will-execute', function (event, obj) {
+				if (!plugin.snaphotInProgress) {
+					plugin.takeSnapshot(obj.editable);
 				}
-				var newValue = aevent.editable.getContents(),
-					patch = dmp.patch_make(oldValue, newValue);
-				// only push an EditCommand if something actually changed.
-				if (0 !== patch.length) {
-					that.stack.execute( new EditCommand( aevent.editable, patch ) );
-				}
+				plugin.snaphotInProgress = true;
+			});
+
+			Aloha.bind('aloha-editable-activated', function () {
+				plugin.undoInProgress = false;
+				plugin.snapshotInProgress = false;
+				plugin.updateButtons();
+
+				// reset undo stack so history is restricted to one editable (and prevent it to grow forever)
+				plugin.stack.commands = [];
+				plugin.stack.stackPosition = plugin.stack.savePosition = -1;
 			});
 		},
 
+		/**
+		 * Take a snapshot of the current editable content.
+		 * @param {Aloha.Editable} editable
+		 */
+		takeSnapshot: function (editable) {
+			if (plugin.undoInProgress) {
+				return;
+			}
+
+			if (typeof editable !== 'object') {
+				editable = Aloha.getActiveEditable();
+			}
+
+			// Store content and bookmark which will be used on the next snapshot.
+			editable.undoSnapshotContent = editable.obj.html();
+			editable.undoSnapshotBookmark = rangy.getSelection().getBookmark(editable.obj[0]);
+		},
 
 		/**
-		 * toString method
-		 * @return string
+		 * Update the content on the editable with the given content and restore the selection.
+		 * @param  {Aloha.Editable} editable
+		 * @param  {String} content
+		 * @param  {Object} bookmark
+		 */
+		setContent: function (editable, content, bookmark) {
+			var data = {
+				editable: editable,
+				content: content
+			};
+
+			plugin.undoInProgress = true;
+			Aloha.trigger('aloha-undo-content-will-change', data);
+
+			editable.obj.html(content);
+			enableBlocks();
+
+			// Restore selection and cursor
+			rangy.getSelection().moveToBookmark(bookmark);
+
+			Aloha.trigger('aloha-undo-content-changed', data);
+			plugin.undoInProgress = false;
+		},
+
+		/**
+		 * Undo the last action if possible.
+		 */
+		undo: function () {
+			if (plugin.stack.canUndo()) {
+				plugin.stack.undo();
+			}
+		},
+
+		/**
+		 * Redo the last action if possible.
+		 */
+		redo: function () {
+			if (plugin.stack.canRedo()) {
+				plugin.stack.redo();
+			}
+		},
+
+		/**
+		 * Create the undo/redo buttons.
+		 */
+		createButtons: function () {
+			plugin.undoButton = Ui.adopt('undo', Button, {
+				tooltip: 'Undo',
+				icon: 'aloha-button-undo',
+				scope: 'Aloha.continuoustext',
+				click: function () {
+					plugin.undo();
+				}
+			});
+
+			plugin.redoButton = Ui.adopt('redo', Button, {
+				tooltip: 'Redo',
+				icon: 'aloha-button-redo',
+				scope: 'Aloha.continuoustext',
+				click: function () {
+					plugin.redo();
+				}
+			});
+
+			plugin.updateButtons();
+		},
+
+		/**
+		 * Update the undo/redo buttons with the proper state.
+		 */
+		updateButtons: function () {
+			plugin.undoButton.element.button('option', 'disabled', !plugin.stack.canUndo());
+			plugin.redoButton.element.button('option', 'disabled', !plugin.stack.canRedo());
+		},
+
+		/**
+		 * toString method.
+		 * @return String
 		 */
 		toString: function () {
 			return 'undo';
-		},
-		undo: function () {
-			if ( null !== Aloha.getActiveEditable() ) {
-				Aloha.getActiveEditable().smartContentChange({type : 'blur'});
-			}
-			this.stack.canUndo() && this.stack.undo();
-		},
-		redo: function () {
-			if ( null !== Aloha.getActiveEditable() ) {
-				Aloha.getActiveEditable().smartContentChange({type : 'blur'});
-			}
-			this.stack.canRedo() && this.stack.redo();
-		},
-		stack: undefined // Defined in init above
-
+		}
 	});
+
+	return plugin;
 });
